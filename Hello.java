c54bfc6c16208d600ae688124fc1bed1c5b53fd8
SK5277-SINGH <singhsushil5277@gmail.com>
2018-08-12 19:14:54 +0530

    add new lines to hello.java file

--- Hello.java
+++ Hello.java
@@ -4,5 +4,8 @@ public class Hello
         for(int i=1; i<=5; i++){
             System.out.println("Hello world.."+i);
         }
+            System.out.println("Finaly congigured with git and jenkins...");
+            System.out.println("I am very happy to see after seeing output...");
+        
     }
 }
